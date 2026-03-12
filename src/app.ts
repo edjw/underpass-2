de66220bd97bf25fcbcd75418fdef6f69bf52ef6
Ed Johnson-Williams <edjw@users.noreply.github.com>
2026-03-12 13:24:43 +0000

    Add Underpass 2 copyright attribution

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,5 @@
 /*  Underpass is Copyright (C) 2021 Markus Noga
+    Underpass 2 is Copyright (C) 2026 Ed Johnson-Williams
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
